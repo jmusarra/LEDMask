38bbc8058f3c3d3071ab72ec6f6768e33e43cd86
jmusarra <github@mightymu.net>
2014-10-18 01:22:13 -0700

    Increased resolution of potentiometer read

--- LEDMask.ino
+++ LEDMask.ino
@@ -21,7 +21,7 @@ const uint32_t red = strip.Color(255, 0, 0);
 const uint32_t blue = strip.Color(0, 0, 255);
 const uint32_t off = strip.Color(0, 0, 0);
 uint8_t brightness = 255;
-uint8_t dimPin = 0;
+uint8_t dimPin = 1;
 uint8_t potLevel = 0;      // doesn't need to be global?
 uint8_t oldLevel = 0;
 float scale = 1.0;
@@ -48,14 +48,14 @@ void setup() {
   strip.begin();
   strip.setBrightness(dimLevel);
   strip.show();                    // Initialize all pixels to 'off'
-  // Serial.begin(9600);           // Useful for troubleshooting and debug - not needed for production version
+  //Serial.begin(9600);           // Useful for troubleshooting and debug - not needed for production version
   
 }
 
 
 uint8_t checkPot() {
   potLevel = map(analogRead(dimPin), 0, 1023, 1, 100);   // set dimmer level to number between 1 and 100
-  if ((potLevel > oldLevel + 2) || (potLevel < oldLevel - 2)) {
+  if ((potLevel > oldLevel) || (potLevel < oldLevel)) {
     oldLevel = potLevel;                                 // store this so we can see if it's changed
     scale = potLevel * 0.01;                             // convert level into decimal
     dimLevel = 250 * scale;                              // and then dimLevel is what we use in setBrightness()
@@ -67,7 +67,7 @@ uint8_t checkPot() {
 void pulse(uint32_t c) {
   uint8_t i, j;
   checkPot();
-  int w = (map(dimLevel, 2, 250, 20, 1));
+  float w = (map(dimLevel, 2, 250, 20, 1));
   
   for (i = 0; i <= strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
@@ -204,6 +204,8 @@ else if (switch9 == HIGH) {
 else {
 solid(off);
   }
+
+
 
 
 
